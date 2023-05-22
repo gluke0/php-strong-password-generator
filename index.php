02815f4c83fb54ecbecf80527dd1928881905ca9
gluke0 <milanesigianluca@gmail.com>
2023-05-22 14:25:10 +0200

    form per lunghezza password

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,9 +9,22 @@
 
     <title>PHP Password Generator</title>
 </head>
-<body>
-    
+<body class="vh-100 bg-primary d-flex justify-content-center">
 
+    <div class="container mt-5">
+        <h1 class="text-uppercase text-center mb-4"> generate your <span class="fs-6"> (hopefully) </span> secure password </h1>
+        <form action="index.php" method="GET">
+            <label class="text-uppercase text-white"> choose the length of your password: </label>
+            <input type="text" name="length" class="form-control w-50 my-4" placeholder="password length">
+            <input type="submit" value="generate" class="btn btn-primary border border-white text-uppercase">
+        </form>
+        <div class="mt-5">
+
+          
+
+        </div>
+    </div>
+    
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>    
 </body>
