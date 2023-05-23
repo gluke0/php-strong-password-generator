8324bc62bdfb71aa346b46c10ea875bb10947d24
gluke0 <milanesigianluca@gmail.com>
2023-05-23 10:03:26 +0200

    checkbox "repeat" e funzione pass funzionanti

--- functions.php
+++ functions.php
@@ -1,15 +1,36 @@
 <?php
 
 if (isset($_GET['length']) && $_GET['length'] > 0) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]:";?/';
-    $password = '';
+ $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]:";?/';
+ $password = '';
 
-    for ($i = 0; $i < $_GET['length']; $i++){
-    $password .= $characters[mt_rand(0, strlen($characters) - 1)];
+if (isset($_GET['repeat']) && $_GET['repeat'] == 'on') {
+    $password = norepeatPassword($characters, $_GET['length']);
+} else {
+    $password = generatePassword($characters, $_GET['length']);
 }
-session_start();
-$_SESSION['password'] = $password;
-header('Location: ./password.php');
+    session_start();
+    $_SESSION['password'] = $password;
+    header('Location: ./password.php');
+}
+
+// mia originale modificata
+function generatePassword($characters, $length) {
+  $strongpassword = '';
+  $alphaLength = strlen($characters) - 1;
+  for ($i = 0; $i < $length; $i++) {
+    $number = rand(0, $alphaLength);
+    $strongpassword .= $characters[$number];
+  }
+  return $strongpassword;
+}
+
+function norepeatPassword($characters, $length) {
+    $strongpassword = '';
+    $norepeat = array_unique(str_split($characters));
+    shuffle($norepeat);
+    $strongpassword = implode('', array_slice($norepeat, 0, $length));
+    return $strongpassword;
 }
 
 ?>
